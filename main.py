4134ca2356229f44020de82b176234dffd7faaa4
k.sidorov <k.sidorov@mgcom.ru>
2025-05-25 17:33:37 +0300

    Йоу

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.model import save_model
+from src.metrics import model_metrics
 from src.builder import detect_anomalies_main, DEFAULT_FEATURES
 from src.data_upload import load_and_preprocess_data
 from src.features import (
@@ -16,12 +17,10 @@ if __name__ == '__main__':
     df = load_and_preprocess_data(r'D:\Work\Diplom\data\raw\transactions.csv')
     df = create_rolling_features(df)
     df = add_geo_features(df)
-
-    df_test, model, scaler, history = detect_anomalies_main(df)
-
+    df_test, model, scaler, history, x_test_scaled = detect_anomalies_main(df, r"D:\Work\Diplom\data\processed\anomalies.csv")
     plot_training_curves(history)
     plot_error_distribution(df_test)
     plot_reconstruction_error_distribution(df_test)
     plot_tsne_latent(model, df_test, scaler, DEFAULT_FEATURES)
-
-    save_model(model, r"C:\Users\k.sidorov\PycharmProjects\Diplom\compiled_models")
+    model_metrics(df_test, model, history, x_test_scaled)
+    save_model(model, r"D:\Work\Diplom\compiled_models\model.keras")
